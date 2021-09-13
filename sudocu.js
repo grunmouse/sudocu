8b072a547359bdb20d67b47f69424eaef4a91b00
Dmitry Rakov <rakov@mirapolis.ru>
2021-09-13 03:30:08 +0300

    process

--- sudocu.js
+++ sudocu.js
@@ -1,3 +1,4 @@
+const inspect = Symbol.for('nodejs.util.inspect.custom');
 const {floor} = Math;
 const {combination} = require('@grunmouse/combinatorics');
 
@@ -9,9 +10,9 @@ function first(itr){
 
 function countItems(sets){
 	let map = new Map();
-	for(let set of sets){
-		//console.log(set);
-		for(let value of set.cell){
+	for(let item of sets){
+		let set = item.cell || item;
+		for(let value of set){
 			let count = !map.has(value) ? 1 : map.get(value)+1;
 			map.set(value, count);
 		}
@@ -19,13 +20,36 @@ function countItems(sets){
 	return map;
 }
 
+class Cell extends Set{
+	constructor(row, col){
+		super([1,2,3,4,5,6,7,8,9]);
+		this.row = row;
+		this.col = col;
+	}
 
+	[inspect](depth, options){
+		//console.log(options);
+		let {row, col} = this;
+		let values = [...this].sort().map(a=>(options.stylize(a, 'number'))).join(', ');
+		
+		
+		return `(${row}, ${col}):  {${values}}`;
+		
+	}
+	
+	toString(){
+		let {row, col} = this;
+		return `(${row}, ${col})`;
+	}
+}
+
+const Array9 = (callback)=>(Array.from({length:9}, callback));
+const Array3 = (callback)=>(Array.from({length:3}, callback));
 
 class Sudocu{
 	constructor(){
 		const fullSet = ()=>(new Set([1,2,3,4,5,6,7,8,9]));
 		const fullMap = ()=>(new Map([1,2,3,4,5,6,7,8,9].map(x=>([x,9]))));
-		const Array9 = (callback)=>(Array.from({length:9}, callback));
 		//Списки недостающих значений
 		this.columns = Array9(fullMap);
 		this.rows = Array9(fullMap);;
@@ -33,9 +57,23 @@ class Sudocu{
 		//Проставленные значения
 		this.values = Array9(()=>(Array9(()=>(NaN))));
 		//Списки разрешённых значений
-		this.cells = Array9(()=>(Array9(fullSet)));
+		this.cells = Array9((_, row)=>(Array9((_, col)=>(new Cell(row, col)))));
+		
+		//Подстроки и подстолбцы - части строк и столбцов, ограниченные квадратами
+		//Набор хранит значения, эксклюзивные для триплета
+		this.subcolumns = Array9(()=>(Array3(()=>(new Set()))));
+		this.subrows = Array9(()=>(Array3(()=>(new Set()))));
+		
+		this.report = [];
 	}
 	
+	clone(){
+		
+	}
+	
+	log(str){
+		this.report.push(str);
+	}
 	/*
 		5*6**3*8*
 		4***8**7*
@@ -107,6 +145,31 @@ class Sudocu{
 		return row * 3 + col;
 	}
 	
+	* itrSubrowsForSquare(index){
+		let hcol = index % 3;
+		let hrow = floor(index/3);
+		for(let i = 0; i<3; ++i){
+			let row = hrow*3 + i;
+			yield {row, hcol, values:this.subrows[row][hcol]};
+		}
+	}
+	* itrSubcolumnsForSquare(index){
+		let hcol = index % 3;
+		let hrow = floor(index/3);
+		for(let i = 0; i<3; ++i){
+			let col = hcol*3 + i;
+			yield {col, hrow, values:this.subcolumns[col][hrow]};
+		}
+	}
+	
+	* itrSubrowsForRow(row){
+		yield* this.subrows[row].map((values, hcol)=>({row, hcol, values}));
+	}
+
+	* itrSubcolumnsForColumn(col){
+		yield* this.subcolumns[col].map((values, hrow)=>({col, hrow, values}));
+	}
+	
 	*itrSquareCells(index){
 		let hcol = index % 3;
 		let hrow = floor(index/3);
@@ -125,7 +188,7 @@ class Sudocu{
 
 	*itrRowCells(row){
 		for(let col = 0; col<9; ++col){
-			yield {col, row, cell:this.cells[row][col]}
+			yield {col, row, cell:this.cells[row][col]};
 		}
 	}
 	
@@ -135,8 +198,31 @@ class Sudocu{
 		yield this.itrSquareCells(this.squareIndex(row, col));
 	}
 	
+	*itrCellsForSubrow(row, hcol){
+		for(let i = 0; i<3; ++i){
+			let col = hcol*3 + i;
+			yield {col, row, cell:this.cells[row][col]};
+		}
+	}
+
+	*itrCellsForSubcolumn(col, hrow){
+		for(let i = 0; i<3; ++i){
+			let row = hrow*3 + i;
+			yield {col, row, cell:this.cells[row][col]};
+		}
+	}
+	
+	itrCellsForTriplet(triplet){
+		if('col' in triplet && 'hrow' in triplet){
+			return this.itrCellsForSubcolumn(triplet.col, triplet.hrow);
+		}
+		else if('row' in triplet && 'hcol' in triplet){
+			return this.itrCellsForSubrow(triplet.row, triplet.hcol);
+		}
+	}
+	
 	hasFinal(){
-		return this.values.every((row)=>(row.values.every((x)=>(!isNaN(x)))));
+		return this.values.every((row)=>(row.every((x)=>(!isNaN(x)))));
 	}
 	
 	setValue(row, col, value){
@@ -151,7 +237,7 @@ class Sudocu{
 		}
 		
 		//Удаляем из списков недостающих
-		for(let map of this.itrMapsForCell(row, col)){
+		for(let {map} of this.itrMapsForCell(row, col)){
 			map.delete(value);
 		}
 		
@@ -161,6 +247,8 @@ class Sudocu{
 				this.deleteOf(item.row, item.col, value);
 			}
 		}
+		
+		this.log(`Ячейка (${row}, ${col}) = ${value}, следовательно, прочие ячейки строки ${row}, столбца ${col} и квадрата ${square} не могут содержать ${value}.`);
 	}
 	
 	/**
@@ -170,7 +258,7 @@ class Sudocu{
 		let cell = this.cells[row][col];
 		if(cell.has(value)){
 			cell.delete(value);
-			for(let map of this.itrMapsForCell(row, col)){
+			for(let {map} of this.itrMapsForCell(row, col)){
 				if(map.has(value)){
 					let count = map.get(value);
 					--count;
@@ -190,15 +278,16 @@ class Sudocu{
 	
 	*itrSetsForCell(row, col){
 		yield this.cells[row][col];
-		yield this.rows[row];
-		yield this.columns[col];
-		yield this.squares[this.squareIndex(row, col)];
+		//yield this.rows[row];
+		//yield this.columns[col];
+		//yield this.squares[this.squareIndex(row, col)];
 	}
 
 	*itrMapsForCell(row, col){
-		yield this.rows[row];
-		yield this.columns[col];
-		yield this.squares[this.squareIndex(row, col)];
+		yield {map:this.rows[row], entityName: `Строка ${row}`};
+		yield {map:this.columns[col], entityName: `Столбец ${col}`};
+		let square = this.squareIndex(row, col);
+		yield {map:this.squares[square], entityName: `Квадрат ${square}`};
 	}
 	
 	/**
@@ -217,7 +306,8 @@ class Sudocu{
 					let ctrl = arr.filter(({cell})=>(!cluster.includes(cell)));
 					let other = new Set(ctrl.map((x)=>([...x.cell])).flat());
 					if(notes.size + other.size > allNotes.size){
-						return {cells:cluster, values:notes};
+						let report = `Ячейки: ${cluster.join(', ')} - неизбежно заключают в себе значения {${[...notes].join(', ')}}.`;
+						return {cells:cluster, values:notes, report};
 					}
 				}
 			}
@@ -236,22 +326,115 @@ class Sudocu{
 	
 	*itrItrAllItrCells(){
 		for(let i = 0; i<9; ++i){
-			yield this.itrRowCells(i);
+			yield {itr:this.itrRowCells(i), entityName:`ячейки строки ${i}`};
 		}
 		for(let i = 0; i<9; ++i){
-			yield this.itrColumnCells(i);
+			yield {itr:this.itrColumnCells(i), entityName:`ячейки столбца ${i}`};
 		}
 		for(let i = 0; i<9; ++i){
-			yield this.itrSquareCells(i);
+			yield {itr:this.itrSquareCells(i), entityName:`ячейки квадрата ${i}`};
 		}
 	}
 	
+	noteForTriplet(triplet){
+		let cells = [...this.itrCellsForTriplet(triplet)];
+		let notes = cells.map((item)=>([...item.cell]));
+		return new Set(notes.flat());
+	}
+	
+	/**
+	 * Анализирует три триплета на эксклюзивные значения
+	 */
+	analyseThreeTriplets(itr){
+		let arr = [...itr];
+		let notes = arr.map((triplet)=>{
+			return this.noteForTriplet(triplet);
+		});
+		let noteCount = countItems(notes);
+		
+		for(let [value, count] of noteCount){
+			if(count === 1){
+				let index = notes.findIndex((x)=>(x.has(value)));
+				let triplet = arr[index];
+				triplet.values.add(value);
+			}
+		}
+	}
+	
+	analyseTriplets(){
+		for(let i = 0; i<9; ++i){
+			this.analyseThreeTriplets(this.itrSubrowsForSquare(i));
+			this.analyseThreeTriplets(this.itrSubcolumnsForSquare(i));
+		}
+	}
+	
+	findUhandledTripletOfThree(itr, entityName){
+		let arr = [...itr];
+		let notes = arr.map((triplet)=>{
+			return this.noteForTriplet(triplet);
+		});
+		let noteCount = countItems(notes);
+		
+		for(let triplet of arr){
+			for(let value of triplet.values){
+				if(noteCount.get(value) > 1){
+					console.log(triplet);
+					let cells = [...this.itrCellsForTriplet(triplet)].map((item)=>(item.cell)).join(', ');
+					let report = `Тройка ячеек: ${cells} - необходимо содержит значение ${value}. Следовательно другие ${entityName} его не содержат.`;
+					return {three:arr, triplet, value, report};
+				}
+			}
+		}
+	}
+	
+	findUnhandledTriplet(){
+		for(let i = 0; i<9; ++i){
+			let result = this.findUhandledTripletOfThree(this.itrSubrowsForRow(i), `ячейки строки ${i}`)
+			if(result){
+				return result;
+			}
+		}
+		for(let i = 0; i<9; ++i){
+			let result = this.findUhandledTripletOfThree(this.itrSubcolumnsForColumn(i), `ячейки столбца ${i}`)
+			if(result){
+				return result;
+			}
+		}
+	}
+	
+	handleTriplet(unhandled){
+		let {three, triplet, value} = unhandled;
+		
+		for(let t of three){
+			if(t != triplet){
+				for(let item of this.itrCellsForTriplet(t)){
+					this.deleteOf(item.row, item.col, value);
+				}
+			}
+		}
+		
+	}
+	
+	handleTripletOne(){
+		this.analyseTriplets();
+		let res = this.findUnhandledTriplet();
+		let f = !!res;
+		
+		if(f){
+			this.log(res.report);
+			this.handleTriplet(res);
+		}
+		return f;
+	}
+	
 	findCluster(){
-		for(let itr of this.itrItrAllItrCells()){
+		for(let {itr, entityName} of this.itrItrAllItrCells()){
 			let arr = [...itr];
 			let cluster = this.findClusterIn(arr);
 			if(cluster){
-				return {source:arr, cluster};
+				//console.log(cluster);
+				let report = cluster.report + ` Следовательно, прочие ${entityName} их не содержат.`
+				return {source:arr, cluster, report};
 			}
 		}
 	}
@@ -261,7 +444,8 @@ class Sudocu{
 		let f = !!res;
 		
 		if(f){
-			let {source, cluster} = res;
+			let {source, cluster, report} = res;
+			this.log(report);
 			this.handleClusterIn(source, cluster);
 		}
 		return f;		
@@ -273,7 +457,18 @@ class Sudocu{
 	findResolve(){
 		for(let row = 0; row<9; ++row) for(let col = 0; col<9; ++col) if(isNaN(this.values[row][col])){
 			let cell = this.cells[row][col];
-			for(let set of this.itrSetsForCell(row, col)){
+			if(cell.size === 1){
+				let value = first(cell);
+				if(Array.isArray(value)){
+					value = value[0];
+				}
+				return {
+					row, col, value,
+					report: `Ячейка (${row}, ${col}) допускает только одно значение - ${value}.`
+				};
+			}
+			
+			/*for(let set of this.itrSetsForCell(row, col)){
 				if(set.size === 1){
 					let value = first(set);
 					if(Array.isArray(value)){
@@ -281,13 +476,16 @@ class Sudocu{
 					}
 					return {row, col, value};
 				}
-			}
-			for(let map of this.itrMapsForCell(row, col)){
+			}*/
+			for(let {map, entityName} of this.itrMapsForCell(row, col)){
 				for(let [value, count] of map){
 					if(count === 1){
 						//console.log('map');
 						if(cell.has(value)){
-							return {row, col, value};
+							return {
+								row, col, value,
+								report: `${entityName} допускает значение ${value} только в позиции (${row}, ${col}).`
+							};
 						}
 					}
 				}
@@ -301,7 +499,9 @@ class Sudocu{
 			f = this.resolveOne();
 			if(!f){
 				f = this.handleClusterOne();
-				if(f){console.log('cluster')};
+			}
+			if(!f){
+				f = this.handleTripletOne();
 			}
 		}
 	}
@@ -311,7 +511,8 @@ class Sudocu{
 		let f = !!res;
 		//console.log(res);
 		if(f){
-			let {row, col, value} = res;
+			let {row, col, value, report} = res;
+			this.log(report);
 			this.setValue(row, col, value);
 		}
 		return f;
--- test.js
+++ test.js
@@ -1,22 +1,31 @@
 const Sudocu = require('./sudocu.js');
 
+//module.exports = function(){
 const sudocu = new Sudocu();
 
-const task = `8**2*6*73
-**6*4****
-**5***9**
-*1*4***6*
-**7******
-****8*1*7
-479*3***5
-*********
-**87**21*
+const task = `*7**29*45
+***158**6
+2**3***9*
+*****1***
+9**24*6*7
+4***6*9*3
+*6******8
+***43****
+*3*5*6*7*
 `;
-
 
 sudocu.parseValues(task);
 
 sudocu.resolve();
 console.log();
+//console.log(sudocu.report.join("\n"));
+console.log();
 console.log(sudocu.printValues());
-console.log(sudocu.cells);
+
+//sudocu.handleTripletOne();
+//sudocu.analyseThreeTriplets(sudocu.itrSubcolumnsForSquare(0));
+
+if(!sudocu.hasFinal()){
+	console.log(sudocu.cells);
+}
+//}
